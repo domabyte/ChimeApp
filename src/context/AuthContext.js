660beff8855278908bcaeab8c0808ad2a19f1e78
doma-byte <singhdikshit69@gmail.com>
2024-04-04 22:13:32 +0530

    feat(AuthContext.js): add AuthContext component to handle user authentication and authorization
    The AuthContext component is responsible for managing user authentication and authorization in the application. It provides a context for storing user information, loading state, error messages, and various authentication-related functions.
    
    The component includes the following features:
    - useState hooks to manage user information, loading state, error messages, and splash loading state.
    - Functions for user registration, OTP verification, login, forgot password, forgot OTP verification, change password, and logout.
    - The isLoggedIn function checks if the user is already logged in by retrieving user information from AsyncStorage.
    - The useEffect hook is used to call the isLoggedIn function when the component mounts.
    
    This commit adds the AuthContext component to the project, allowing other components to access and utilize the authentication and authorization functionalities provided by the context.

---
+++ src/context/AuthContext.js
@@ -0,0 +1,217 @@
+import React, {createContext, useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import config from '../config/config';
+import axios from 'axios';
+export const AuthContext = createContext();
+
+export const AuthProvider = ({children}) => {
+  const [userInfo, setUserInfo] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [splashLoading, setSplashLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const register = async (
+    firstName,
+    lastName,
+    email,
+    newPassword,
+    confirmPassword,
+    encryptedCaptcha,
+    confirmCaptcha,
+    isChecked,
+    navigationCallback,
+  ) => {
+    setIsLoading(true);
+    const queryString =
+      config.registrationURL +
+      `?Mem_Name=${firstName}&Mem_LName=${lastName}&Email=${email}&Password=${newPassword}&ConfirmPassword=${confirmPassword}&Captcha=${encryptedCaptcha}&UserCaptcha=${confirmCaptcha}&termcondition=${isChecked}`;
+    try {
+      const {data} = await axios.post(queryString);
+      if (data.Mem_ID && data.Mem_ID > 0) {
+        navigationCallback(data.Mem_ID, data.message);
+      } else {
+        setError(data.errorText || 'Registration failed.');
+      }
+    } catch (err) {
+      setError(`Registration error: ${error}`);
+      Alert.alert('Registration Error', `An error occurred: ${error.message}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const otpVerify = async (Mem_ID, otpString) => {
+    setIsLoading(true);
+    if (otpString.length !== 6) {
+      setError('OTP must be 6 digits');
+      setIsLoading(false);
+      return;
+    }
+    const [OTP1, OTP2, OTP3, OTP4, OTP5, OTP6] = otpString.split('');
+
+    const queryString = `${config.otpRegistrationURL}?Mem_Id=${Mem_ID}&OTP1=${OTP1}&OTP2=${OTP2}&OTP3=${OTP3}&OTP4=${OTP4}&OTP5=${OTP5}&OTP6=${OTP6}`;
+    try {
+      const {data} = await axios.post(queryString);
+      if (data.Mem_ID && data.Mem_ID > 0) {
+        const value = {
+          name: data.Mem_Name + ' ' + data.Mem_LName,
+          id: data.Mem_ID,
+          friendid: data.FriendId,
+          memberToken: data.MemberToken,
+          LoginToken: data.LoginToken,
+        };
+        let userInfo = value;
+        setUserInfo(userInfo);
+        AsyncStorage.setItem('userInfo', JSON.stringify(userInfo));
+        setIsLoading(false);
+      } else {
+        setError(data.message);
+        setIsLoading(false);
+      }
+    } catch (err) {
+      setIsLoading(false);
+      console.log(err.error);
+    }
+  };
+
+  const login = async (email, password) => {
+    setIsLoading(true);
+    const url = config.loginURL + email + '&password=' + password;
+    try {
+      const {data} = await axios.get(url);
+      if (data.Mem_ID && data.Mem_ID > 0) {
+        const value = {
+          name: data.Mem_Name + ' ' + data.Mem_LName,
+          id: data.Mem_ID,
+          friendid: data.FriendId,
+          memberToken: data.MemberToken,
+          LoginToken: data.LoginToken,
+        };
+        let userInfo = value;
+        setUserInfo(userInfo);
+        AsyncStorage.setItem('userInfo', JSON.stringify(userInfo));
+        setIsLoading(false);
+      } else {
+        setError(data.message);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+
+  const forgotPassword = async email => {
+    setIsLoading(true);
+    const url = config.forgotPassURL + '?mailid=' + email;
+    try {
+      const {data} = await axios.post(url);
+      setIsLoading(false);
+      if (data.Mem_ID && data.Mem_ID > 0) {
+        const value = {
+          id: data.Mem_ID,
+          message: data.message,
+          email,
+        };
+        return value;
+      }
+      return false;
+    } catch (err) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+
+  const forgotOTPVerification = async (Mem_ID, otpString) => {
+    setIsLoading(true);
+    if (otpString.length !== 6) {
+      setError('OTP must be 6 digits');
+      setIsLoading(false);
+      return;
+    }
+    const [OTP1, OTP2, OTP3, OTP4, OTP5, OTP6] = otpString.split('');
+
+    const queryString = `${config.forgotOTPVerifyURL}?Mem_Id=${Mem_ID}&OTP1=${OTP1}&OTP2=${OTP2}&OTP3=${OTP3}&OTP4=${OTP4}&OTP5=${OTP5}&OTP6=${OTP6}`;
+    try {
+      const {data} = await axios.post(queryString);
+      setIsLoading(false);
+      if (data.Mem_ID && data.Mem_ID > 0) {
+        return true;
+      } else {
+        setError(data.message);
+        setIsLoading(false);
+      }
+    } catch (err) {
+      setIsLoading(false);
+      console.log(err.error);
+    }
+  };
+
+  const changePassword = async (Mem_ID, password, confirmPassword) => {
+    setIsLoading(true);
+    try {
+      const {data} = await axios.post(config.resetPasswordURL, {
+        Mem_ID: Mem_ID,
+        New_password: password,
+        Confirm_password: confirmPassword,
+      });
+      setIsLoading(false);
+      return data.code === 1 ? true : setError(data.message);
+    } catch (err) {
+      setIsLoading(false);
+      console.log(err.error);
+    }
+  };
+
+  const logout = () => {
+    setIsLoading(true);
+    try {
+      AsyncStorage.removeItem('userInfo');
+      setUserInfo({});
+      setIsLoading(false);
+      setError(null);
+    } catch (err) {
+      console.log(`logout error ${e}`);
+      setIsLoading(false);
+    }
+  };
+
+  const isLoggedIn = async () => {
+    try {
+      setSplashLoading(true);
+
+      let userInfo = await AsyncStorage.getItem('userInfo');
+      userInfo = JSON.parse(userInfo);
+
+      if (userInfo) {
+        setUserInfo(userInfo);
+      }
+      setSplashLoading(false);
+    } catch (e) {
+      setSplashLoading(false);
+      console.log(`is logged in error ${e}`);
+    }
+  };
+  useEffect(() => {
+    isLoggedIn();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoading,
+        userInfo,
+        splashLoading,
+        error,
+        register,
+        otpVerify,
+        login,
+        forgotPassword,
+        forgotOTPVerification,
+        changePassword,
+        logout,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
