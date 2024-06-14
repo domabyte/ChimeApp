21cdd33aec3bdb82d0093da43f02560e9d7b5011
doma-byte <singhdikshit69@gmail.com>
2024-06-14 14:52:11 +0530

    feat(index.js): import required libraries for call handling and push notifications feat(package.json): add dependencies for call handling, push notifications, and sound libraries [For the first commit message]: added support for call handling and push notifications in the application [For the second commit message]: imported necessary libraries and configured them for use in the application

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import "./src/utils/CallDetect";
+import RNCallKeep from 'react-native-callkeep';
 
+const options = {
+  ios: {
+    appName: 'ACTPAL',
+  },
+  android: {
+    alertTitle: 'Permissions required',
+    alertDescription: 'This application needs to access your phone accounts',
+    cancelButton: 'Cancel',
+    okButton: 'ok',
+    imageName: 'actpal',
+    foregroundService: {
+      channelId: 'com.actpal',
+      channelName: 'Foreground service for my app',
+      notificationTitle: 'My app is running on background',
+      notificationIcon: 'Path to the resource icon of the notification',
+    },
+  },
+};
+RNCallKeep.setup(options);
+RNCallKeep.setAvailable(true);
 AppRegistry.registerComponent(appName, () => App);
