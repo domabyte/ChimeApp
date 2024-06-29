998b4e26a57fc48bbd5a579ec7c5016cf28d009c
doma-byte <singhdikshit69@gmail.com>
2024-06-29 17:15:21 +0530

    refactor(ios/RNVideoViewManager.m): add RCTUIManager import and export UIView property 'mirror' and method 'setMirror:' for mirror property handling in DefaultVideoRenderView.

--- ios/RNVideoViewManager.m
+++ ios/RNVideoViewManager.m
@@ -1,16 +1,28 @@
 #import "RNVideoViewManager.h"
+#import <React/RCTUIManager.h>
 
 @implementation RNVideoViewManager
-static NSMutableDictionary<NSString*, NSValue*> *videoMap;
 
-RCT_EXPORT_MODULE(RNVideoView);
+RCT_EXPORT_MODULE(RNVideoView)
 
 - (UIView *)view
 {
-  DefaultVideoRenderView *innerView = [[DefaultVideoRenderView alloc] init];
-  innerView.contentMode = UIViewContentModeScaleAspectFit;
-  return innerView;
+    DefaultVideoRenderView *innerView = [[DefaultVideoRenderView alloc] init];
+    innerView.contentMode = UIViewContentModeScaleAspectFit;
+    return innerView;
+}
+
+RCT_EXPORT_VIEW_PROPERTY(mirror, BOOL)
+
+RCT_EXPORT_METHOD(setMirror:(nonnull NSNumber *)reactTag mirror:(BOOL)mirror)
+{
+    [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, NSDictionary<NSNumber *,UIView *> *viewRegistry) {
+        UIView *view = viewRegistry[reactTag];
+        if ([view isKindOfClass:[DefaultVideoRenderView class]]) {
+            DefaultVideoRenderView *videoView = (DefaultVideoRenderView *)view;
+            videoView.mirror = mirror;
+        }
+    }];
 }
 
 @end
-
